045cb6b9dabbfcec4da2cd6f67ff3209bf57bab7
Digonzalezc <danyg2406@gmail.com>
2023-02-07 12:36:10 -0500

    primer comit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import '../css/Login.css';
 function Login() {
   return (
-    <>
-      <div className="mb-3 row">
-        <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
-        <div className="col-sm-10">
-          <input type="text" className="form-control-plaintext" value="email@example.com"/>
+    <div className='containerPrincipal'>
+      <div className='containerSecundario'>
+        <div className='form-group'>
+          <label>Usuario: </label>
+          <br></br>
+          <input type='text' className='form-control'></input>
+          <br></br>
+          <label>Contraseña: </label>
+          <br></br>
+          <input type='password' className='form-control'></input>
+          <br></br>
+          <button className='btn btn-primary'>Iniciar Sesion</button>
         </div>
       </div>
-      <div className="mb-3 row">
-        <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
-        <div className="col-sm-10">
-          <input type="password" className="form-control" id="inputPassword"/>
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
 
