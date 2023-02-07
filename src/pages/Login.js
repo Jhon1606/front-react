953219916b35648ea74b2fc3d50544cad4e24469
Jhon1606 <barrosjhon936@gmail.com>
2023-02-07 18:00:56 -0500

    Login creado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import '../css/Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 function Login() {
   return (
-    <div className='containerPrincipal'>
-      <div className='containerSecundario'>
-        <div className='form-group'>
-          <label>Usuario: </label>
-          <br></br>
-          <input type='text' className='form-control'></input>
-          <br></br>
-          <label>Contraseña: </label>
-          <br></br>
-          <input type='password' className='form-control'></input>
-          <br></br>
-          <button className='btn btn-primary'>Iniciar Sesion</button>
+    <div class="container w-75 bg-primary mt-5 rounded shadow">
+      <div class="row align-items-stretch">
+        <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded"></div>
+        <div class="col bg-white p-5 rounded-end" >
+          <div class="text-end">
+              <img src="" alt=""/>
+          </div>
+          <h2 class="fw-bold text_center py-5 text-center p-3">Inicio de sesión</h2>
+          <form action="" method="POST">
+              <div class="mb-4">
+                  <label for="usuario" class="form-label"><i class="bi bi-person"></i> Usuario</label>
+                  <input type="text" name="usuario" placeholder="Usuario..." class="form-control"/>
+              </div>
+              <div class="mb-4">
+                  <label for="password" class="form-label"><i class="bi bi-lock"></i> Contraseña</label>
+                  <input type="password" name="password" placeholder="Contraseña..." class="form-control"/>
+              </div>
+              <div class="d-grid">
+                  <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+              </div>
+          </form>
         </div>
-      </div>
+    </div>
     </div>
   )
 }
